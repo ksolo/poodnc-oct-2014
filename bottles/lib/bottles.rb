fa80600cd32071795836c01ebbdfe1813ee17e1f
Kevin Solorio <kevin@detone8.com>
2014-10-30 11:43:35 -0400

    updated cases to include 1 - 99

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -11,10 +11,24 @@ class Bottles
     case number
     when 0
       "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-    when 1
-      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
     else
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number-1} #{container(number-1)} of beer on the wall.\n"
+      "#{number} #{container(number)} of beer on the wall, #{number} #{container(number)} of beer.\nTake #{pronoun(number)} down and pass it around, #{remaining_beverages(number - 1)} #{container(number - 1)} of beer on the wall.\n"
+    end
+  end
+
+  def remaining_beverages(num=0)
+    if num == 0
+      "no more"
+    else
+      num.to_s
+    end
+  end
+
+  def pronoun(num=1)
+    if num == 1
+      "it"
+    else
+      "one"
     end
   end
 
